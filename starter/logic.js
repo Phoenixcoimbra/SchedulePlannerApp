889199fb2200f4a2ef0d29ce0db6577e49f6baa5
Phoenixcoimbra <igormcoimbra@gmail.com>
2024-01-16 23:31:31 +0000

    added the time blocks html

---
+++ starter/logic.js
@@ -0,0 +1,10 @@
+// Front-end environment with ES6 modules
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+dayjs.extend(customParseFormat);
+
+// Display the current date at the top of the page
+const date = dayjs().format('dddd, MMMM D, YYYY');
+document.getElementById('date').innerHTML = date;
+
+
